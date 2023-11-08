457a38963655fb92d631b340a95f6a63887190d5
Graeme Acheson <graeme@sports4cast.com>
2023-11-08 18:47:54 +0000

    intital deploy

---
+++ SW E-Racing.py
@@ -0,0 +1,58 @@
+import pandas as pd
+import streamlit as st
+from streamlit_autorefresh import st_autorefresh
+import sys
+
+sys.path.append('/Users/achie188/Library/CloudStorage/GitHub/4CC/SLT')
+sys.path.append('/Users/achie188/Library/CloudStorage/GitHub/4CC/SLT/input')
+sys.path.append('/Users/achie188/Library/CloudStorage/GitHub/4CC/SLT/pipeline')
+
+
+from pull_zwift import pull_zwift
+
+#3921745
+event_id = '3917221'
+
+#Test event
+t1 = pull_zwift(event_id)
+
+
+interval=5 * 1000
+
+#rename columns
+t1 = t1.rename(columns={'position': '#'})
+t1 = t1.rename(columns={'heartRateInBpm': 'HR'})
+t1 = t1.rename(columns={'powerOutputInWatts': 'Watts'})
+t1 = t1.rename(columns={'powerInWattsPerKg': 'Watts/Kg'}).round(2)
+t1 = t1.rename(columns={'liveTimeGapToLeaderInSeconds': 'Gap'})
+t1 = t1.rename(columns={'completionTimeInSeconds': 'Finish Time'})
+t1 = t1.rename(columns={'distanceInMeters': 'Distance'}).round(1)
+t1 = t1.rename(columns={'speedInKmHours': 'Speed'}).round(1)
+t1 = t1.rename(columns={'powerUpUsed': 'PowerUps Used'})
+
+
+
+# Set up Streamlit app
+st.set_page_config(
+    page_title="SW E-Racing",
+    layout="wide"
+)
+
+st_autorefresh(interval, limit=1000)
+
+st.markdown("""
+        <style>
+               .block-container {
+                    padding-top: 1rem;
+                }
+        </style>
+        """, unsafe_allow_html=True)
+
+st.subheader("Welcome to the Sexy Walrus E-Racing Series 2023! 👋")
+
+st.write("The below show live data from the Sexy Walrus E-Racing Series - Stage 1.")
+
+if t1.empty:
+    st.write("Race not started yet, no data. Please be patient.")
+else:
+    st.dataframe(t1, height=2000, hide_index=True)
---
+++ pull_zwift.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import requests
+import streamlit as st
+
+AUTH_TOKEN = st.secrets["zwift_credentials"]["AUTH_TOKEN"]
+
+
+
+def pull_zwift(event_id):
+    url = "https://us-or-rly101.zwift.com/relay/race/events/" + str(event_id) + "/placement?from=1&to=50&chipTime=true"
+
+    payload = {}
+    headers = {
+        'Authorization': f'Bearer {AUTH_TOKEN}'
+    }
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+
+    data = response.json()
+
+    # Extract 'placement' details
+    placement_data = data.get('placement', [])
+
+    race_data = pd.DataFrame(placement_data)
+
+    if not(race_data).empty:
+        race_data.drop(columns=['jerseyHash', 'location', 'countryCodeAlpha2Code', 'crossingStartingLineGap', 'rideOnsCounter', 'arrivalAtInSeconds'], inplace=True)
+
+    return race_data
+
+

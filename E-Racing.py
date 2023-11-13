9edcf93e17458e09962a08b0b6ad7af83192ad7e
Graeme Acheson <graeme@sports4cast.com>
2023-11-13 14:20:39 +0000

    rules

--- E-Racing.py
+++ E-Racing.py
@@ -129,18 +129,72 @@ with tab4:
     tab41, tab42, tab43, tab44, tab45 = st.tabs(['Walrus E-Racing', 'Lego Boots', 'Amazon Beaconsfield Services', 'Tesla Thames Water', 'AstraZenaca Trailfinders'])
 
     with tab41:
-        st.subheader('Walrus E-Racing 2023')
-        st.write('''Sexy Walrus e-racing league 2023
-
-It’s the fireworks. That’s how you can tell. The Series has started. Even now people are out there, defying the odds, dealing with all sorts of mechanical issues, pairing problems, mental deficiencies, and serious lung diseases. Busting their guts out all over the 7.2km parcours of the prologue. 
-
-For those with bad memories, or for others who consciously choose not to remember,   the 2022 sexy walrus e-racing league wrapped up with Equine Onlyfans walking away with the top spot in the series. But it was Dovistar’s Oli Dammone who took the Orange Jersey and was awarded a fetching casquette as a result. 
-
-Routes Coffee TZA Draconi’s Al Bath ran hot and cold all series, but did enough to snatch not only the sprinter’s jersey but also the climber’s too. A basket full of gels was his just reward. 
-
-But time moves on, bellies increase in diameter, and e-cycling teams struggle for cash. 
-
-The cycling world has had a bloodied year, and none of the teams from last year have survived into 2023. But this void have created space for some smaller UK companies to venture into cycling, with the cash to fund some exciting new teams for 2023.''')
+        st.subheader('Sexy Walrus e-racing league 2023')
+        st.write(''' Sexy Walrus Autumn 2023 e-racing League
+ 1. Overview
+ 1.1 There will be 5 weeks of action. Each week will have one or two races. Details of what each week will entail will be released in good time before each week.
+1.2. Here is a summary of what is in store:
+Prologue: Individual time trial. 7.3km. Done on handicapped bikes. Route is the start of Road to Ruins, ending on the climb at a virtual banner. (Keen eyes will spot this is the same prologue as last year. This allows some fine tuning of handicaps prior to the real racing.
+Weeks 1, 2, 3 and 4: classic Zwift racing. 30-45 mins. Each week the course will be chosen by a different team.
+Team Time Trial: Circa 40-45 min race. The time for this TTT is TBC.
+Week 5: a longer race on an attritional course, serving as the grand finale. (Circa 55 mins).
+1.3 There will be organised breakaways for weeks 1 to 4. There will also be ‘designated sprinters’ or ‘designated climbers’ for these weeks. And the usual orange jersey competition will be happening.
+2. Howtowin?
+2.1 The league will be won by the team that amasses the most points over the course of the season.
+2.2. See later on in this document for a summary of how the points system works.
+3. Breakaways
+3.1. Some races will have a breakaway. This will work by 3 riders being chosen to form a breakaway.
+3.2. There will be 4 races which will feature a Break. For each of these 4 races, each of 3 teams will select a rider (in secret) to be in the break. Therefore one team will miss out on the break each time.
+3.3. The 3 person breakaway will then set off a set time interval before the main bunch.
+  
+ 3.4. The team that will miss the breakaway will be decided randomly. But each team will only miss out on 1 breakaway during the season.
+4. Designatedsprinter/designatedclimber
+4.1. For each race (other than the TTT), each team will be able to designate either a sprinter, or a climber. They must declare their designated sprinter/climber publicly at least 30 minutes before the start of the stage.
+4.2. If a designated sprinter is chosen, then any intermediate sprint points that that rider acquires are doubled. And any finish line points (on stages that are classified as sprint finishes) that that rider acquires are increased by 20%.
+4.3. If a designated climber is chosen, then any KOM points that that rider acquires are doubled. And any finish line points (on stages that are classified as hill top finishes) that that rider acquires are increased by 20%.
+4.4. A rider may only be a designated sprinter or designated climber once. (For example, if a rider is the designated climber in week 1, they will not be allowed to be the designated climber or the designated sprinter for any other races).
+5. THEPOINTSSYSTEMEXPLAINED
+5.1. Each week there will be a points pot, all of which will be distributed out across three different categories (Finishing position, Intermediate sprints, KOM points). The categories are explained below. Each different category will have a % of the points pot allocated to it, which may be different each week.
+For example, a race that had points solely for finishing position would have 100% of the points for the ‘Finishing Position’ category, and 0% for the other categories. A race that was all about intermediate sprinting might have 70% of the points pot allocated to the ‘intermediate sprints’ category, and 30% allocated to finishing position.
+5.2. The prologue will have 500 points available in the points pot. Weeks 1 through to and including week 5 will have 1,000 points in the pot. And the final week will have 2,000 points.
+5.3. In addition to the above (Ie outside of the points pots), further points are available for the Orange Jersey, most aggressive rider, and race reports (see below).
+6. Category1:Finishingposition
+     
+ 6.1. These points will be awarded to the riders on a % of the category pot basis (Ie. a % of the allocated Finishing Position %) . Whereby the higher the finishing position, the higher % of the pot you receive. See Schedule 3 for the percentage distributions.
+6.2. As an example, if you come first you would get 10% of the pot. But if you came 21st you would get 2.1% of the pot.
+6.3. However, if there are fewer than 40 riders racing, then not all the pot will be distributed out (as the lower placings will now not be filled), leaving a ‘remainder’ amount.
+6.4. This ‘remainder’ (whatever it may be) will be reallocated to the riders equally amongst all riders that raced.
+7. Category 2: KOM points
+7.1. These may be awarded on certain races but not necessarily every race.
+7.2. The points allocated will be determined on a race by race basis, as it will depend on the severity of the climb it relates to
+8. Category 3: Intermediate Sprint points
+8.1. The points allocated will be determined on a race by race basis.
+8.2. Details of the intermediate sprints and KOM will be made clear in advance of the relevant race.
+9. Most aggressive rider
+9.1. Twenty points available each week to the most aggressive rider, as chosen by agreement by team leaders. (Things that might win you this award are activities such as going on a wild hopeless attack that is doomed to fail, and inevitably does fail.)
+9.2. Only one MAR per night
+9.3. Most aggressive rider over the course of the season will be awarded 100 points after the conclusion of the final week’s race.
+10. Race reports
+Twenty points will be awarded to the team of any rider that submits a race report after each week, that is deemed to be ‘up to snuff’ by agreement of the team captains.
+    
+ 11. Orange jersey competition
+11.1. Points available at the end of each week (starting after the conclusion of Week 1 - Ie. No points awarded after the Prologue) for rider’s position in Orange jersey leaderboard. A rider must have finished a race to be awarded any points.
+11.2. Points available at the end of the season for the top 10 in the orange jersey leaderboard. 11.3. See Schedules 1 and 2 below for the points available.
+11.4. The Orange Jersey leaderboard tracks overall time over each stage. (Otherwise known as General Classification.)
+11.5. If a rider does not start, or does not finish, a stage, then their time taken will be the greater of (i) 5 minutes slower than the fastest finisher for that stage, and (ii) 30 seconds slower than the slowest finisher on that stage.
+Orange passes
+11.6. Each team will have 6 Orange Passes. A team can use an Orange Pass on any given stage for any rider that has either:
+(i) not started that stage, or
+(ii) has had a technical problem meaning that they could not complete that stage.
+11.7. Where an Orange Pass is used, instead of calculating their time as per 11.5 above that rider will receive the slower of:
+(i) the same time as the second slowest rider in their team; and
+(ii) 90s slower than the rider who finishes first in that race.
+11.8. If a rider uses an Orange Pass, they will not receive any Orange Jeresy points for that week, except for the final week, in which case that rider will receive points for their final finishing position (if they so qualify)..
+12. Quibbling
+Any quibbling of any nature by any of the riders or wider team members may result in a fine up to €1,500 and/or a rider being given last place for the race or stage to which the quibbling relates.
+13. Director’s decision
+A race director’s decision, no matter how erratic, inconsistent or unlawful, shall be binding, final and shall not be quibbled with. In case of any quibbling, see Rule 12. 
+  ''')
 
     with tab42:
         st.subheader('Lego pours millions into doomed vanity project')

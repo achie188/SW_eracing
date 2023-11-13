8d4621f5037fe71338d2aef0dd8bcfce43d7f807
Graeme Acheson <graeme@sports4cast.com>
2023-11-13 20:55:05 +0000

    update

--- pipeline/formatting.py
+++ pipeline/formatting.py
@@ -116,7 +116,7 @@ def teams_slice(df, team, handicaps, columns_to_replace):
 
     sum_columns = ['#', 'Weight']
     columns_to_sum = sum_columns + columns_to_replace
-    total_row = df[columns_to_sum].sum(axis=0)
+    total_row = filtered_df[columns_to_sum].sum(axis=0)
 
     filtered_df = filtered_df.append(total_row, ignore_index=True)
 

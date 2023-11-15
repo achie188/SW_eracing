0f9ef97d51af6e255983eb78dfb11b0d96e5645e
Graeme Acheson <graeme@sports4cast.com>
2023-11-15 18:48:23 +0000

    update

--- inputs/rules.py
+++ inputs/rules.py
@@ -7,12 +7,14 @@ def rules():
 
     1.2. Here is a summary of what is in store:  
 
-    - Prologue: Individual time trial. 7.3km. Done on handicapped bikes. Route is the start of Road to Ruins, ending on the climb at a virtual banner. (Keen eyes will spot this is the same prologue as last year. This allows some fine tuning of handicaps prior to the real racing. 
+    - Prologue: Individual time trial. 7.3km. Done on handicapped bikes. Route is the start of Road to Ruins, ending on the climb at a virtual banner. (Keen eyes will 
+    spot this is the same prologue as last year. This allows some fine tuning of handicaps prior to the real racing. 
     - Weeks 1, 2, 3 and 4: classic Zwift racing. 30-45 mins. Each week the course will be chosen by a different team. 
     - Team Time Trial: Circa 40-45 min race. The time for this TTT is TBC. 
     - Week 5: a longer race on an attritional course, serving as the grand finale. (Circa 55 mins). 
 
-    1.3. There will be organised breakaways for weeks 1 to 4. There will also be ‘designated sprinters’ or ‘designated climbers’ for these weeks. And the usual orange jersey competition will be happening.
+    1.3. There will be organised breakaways for weeks 1 to 4. There will also be ‘designated sprinters’ or ‘designated climbers’ for these weeks. And the usual orange 
+    jersey competition will be happening.
     '''
 
     r2_how_to_win = '''
@@ -27,7 +29,8 @@ def rules():
     #### 3. Breakaways
     3.1. Some races will have a breakaway. This will work by 3 riders being chosen to form a breakaway. 
 
-    3.2. There will be 4 races which will feature a Break. For each of these 4 races, each of 3 teams will select a rider (in secret) to be in the break. Therefore one team will miss out on the break each time. 
+    3.2. There will be 4 races which will feature a Break. For each of these 4 races, each of 3 teams will select a rider (in secret) to be in the break. Therefore one 
+    team will miss out on the break each time. 
 
     3.3. The 3 person breakaway will then set off a set time interval before the main bunch. 
 
@@ -36,28 +39,36 @@ def rules():
 
     r4_designated_sprinter_climber = '''
     #### 4. Designated Sprinter/Climber
-    4.1. For each race (other than the TTT), each team will be able to designate either a sprinter, or a climber. They must declare their designated sprinter/climber publicly at least 30 minutes before the start of the stage. 
+    4.1. For each race (other than the TTT), each team will be able to designate either a sprinter, or a climber. They must declare their designated sprinter/climber 
+    publicly at least 30 minutes before the start of the stage. 
 
-    4.2. If a designated sprinter is chosen, then any intermediate sprint points that that rider acquires are doubled. And any finish line points (on stages that are classified as sprint finishes) that that rider acquires are increased by 20%. 
+    4.2. If a designated sprinter is chosen, then any intermediate sprint points that that rider acquires are doubled. And any finish line points (on stages that are 
+    classified as sprint finishes) that that rider acquires are increased by 20%. 
 
-    4.3. If a designated climber is chosen, then any KOM points that that rider acquires are doubled. And any finish line points (on stages that are classified as hill top finishes) that that rider acquires are increased by 20%. 
+    4.3. If a designated climber is chosen, then any KOM points that that rider acquires are doubled. And any finish line points (on stages that are classified as hill 
+    top finishes) that that rider acquires are increased by 20%. 
 
-    4.4. A rider may only be a designated sprinter or designated climber once. (For example, if a rider is the designated climber in week 1, they will not be allowed to be the designated climber or the designated sprinter for any other races). 
+    4.4. A rider may only be a designated sprinter or designated climber once. (For example, if a rider is the designated climber in week 1, they will not be allowed to 
+    be the designated climber or the designated sprinter for any other races). 
     '''
 
     r5_points = '''
     #### 5. THE POINTS SYSTEM EXPLAINED
-    5.1. Each week there will be a points pot, all of which will be distributed out across three different categories (Finishing position, Intermediate sprints, KOM points). The categories are explained below. Each different category will have a % of the points pot allocated to it, which may be different each week.
-    For example, a race that had points solely for finishing position would have 100% of the points for the ‘Finishing Position’ category, and 0% for the other categories. A race that was all about intermediate sprinting might have 70% of the points pot allocated to the ‘intermediate sprints’ category, and 30% allocated to finishing position. 
+    5.1. Each week there will be a points pot, all of which will be distributed out across three different categories (Finishing position, Intermediate sprints, KOM points). 
+    The categories are explained below. Each different category will have a % of the points pot allocated to it, which may be different each week.
+    For example, a race that had points solely for finishing position would have 100% of the points for the ‘Finishing Position’ category, and 0% for the other categories. 
+    A race that was all about intermediate sprinting might have 70% of the points pot allocated to the ‘intermediate sprints’ category, and 30% allocated to finishing position. 
 
-    5.2. The prologue will have 500 points available in the points pot. Weeks 1 through to and including week 5 will have 1,000 points in the pot. And the final week will have 2,000 points. 
+    5.2. The prologue will have 500 points available in the points pot. Weeks 1 through to and including week 5 will have 1,000 points in the pot. And the final week will 
+    have 2,000 points. 
 
     5.3. In addition to the above (Ie outside of the points pots), further points are available for the Orange Jersey, most aggressive rider, and race reports (see below). 
     '''
      
     r6_cat1 = '''
     ##### 6. Category 1: Finishing Position Points
-    6.1. These points will be awarded to the riders on a % of the category pot basis (Ie. a % of the allocated Finishing Position %) . Whereby the higher the finishing position, the higher % of the pot you receive. See Schedule 3 for the percentage distributions. 
+    6.1. These points will be awarded to the riders on a % of the category pot basis (Ie. a % of the allocated Finishing Position %) . Whereby the higher the finishing 
+    position, the higher % of the pot you receive. See Schedule 3 for the percentage distributions. 
 
     6.2. As an example, if you come first you would get 10% of the pot. But if you came 21st you would get 2.1% of the pot. 
 
@@ -82,7 +93,8 @@ def rules():
 
     r9_mar = '''           
     #### 9. Most Aggressive Rider
-    9.1. Twenty points available each week to the most aggressive rider, as chosen by agreement by team leaders. (Things that might win you this award are activities such as going on a wild hopeless attack that is doomed to fail, and inevitably does fail.) 
+    9.1. Twenty points available each week to the most aggressive rider, as chosen by agreement by team leaders. (Things that might win you this award are activities such 
+    as going on a wild hopeless attack that is doomed to fail, and inevitably does fail.) 
 
     9.2. Only one MAR per night 
 
@@ -96,13 +108,15 @@ def rules():
         
     r11_orange = '''                
     #### 11. Orange Jersey Competition
-    11.1. Points available at the end of each week (starting after the conclusion of Week 1 - Ie. No points awarded after the Prologue) for rider’s position in Orange jersey leaderboard. A rider must have finished a race to be awarded any points. 
+    11.1. Points available at the end of each week (starting after the conclusion of Week 1 - Ie. No points awarded after the Prologue) for rider’s position in Orange jersey 
+    leaderboard. A rider must have finished a race to be awarded any points. 
 
     11.2. Points available at the end of the season for the top 10 in the orange jersey leaderboard. 11.3. See Schedules 1 and 2 below for the points available. 
 
     11.4. The Orange Jersey leaderboard tracks overall time over each stage. (Otherwise known as General Classification.) 
 
-    11.5. If a rider does not start, or does not finish, a stage, then their time taken will be the greater of (i) 5 minutes slower than the fastest finisher for that stage, and (ii) 30 seconds slower than the slowest finisher on that stage. 
+    11.5. If a rider does not start, or does not finish, a stage, then their time taken will be the greater of (i) 5 minutes slower than the fastest finisher for that stage,
+      and (ii) 30 seconds slower than the slowest finisher on that stage. 
 
     ##### Orange passes 
     11.6. Each team will have 6 Orange Passes. A team can use an Orange Pass on any given stage for any rider that has either: 
@@ -113,27 +127,32 @@ def rules():
     - (i) the same time as the second slowest rider in their team; and 
     - (ii) 90s slower than the rider who finishes first in that race. 
 
-    11.8. If a rider uses an Orange Pass, they will not receive any Orange Jeresy points for that week, except for the final week, in which case that rider will receive points for their final finishing position (if they so qualify).
+    11.8. If a rider uses an Orange Pass, they will not receive any Orange Jeresy points for that week, except for the final week, in which case that rider will receive points 
+    for their final finishing position (if they so qualify).
     '''     
 
     r12_quibble = '''           
     #### 12. QUIBBLING
-    Any quibbling of any kind and of any nature by any of the riders or wider team members and their extended families may result in a fine up to €1,500 and/or a rider being given last place for the race or stage to which the quibbling relates. 
+    Any quibbling of any kind and of any nature by any of the riders or wider team members and their extended families may result in a fine up to €1,500 and/or a rider being 
+    given last place for the race or stage to which the quibbling relates. 
     ''' 
 
     r13_dd = '''           
     #### 13. Director’s Decision
-    A race director’s decision, no matter how erratic, inconsistent, unfair or unlawful, shall be binding, final and shall not be quibbled with. In the case of any quibbling, see Rule 12. 
+    A race director’s decision, no matter how erratic, inconsistent, unfair or unlawful, shall be binding, final and shall not be quibbled with. In the case of any quibbling, 
+    see Rule 12. 
     '''
 
     handicaps = '''
     #### Handicapping System
 
-    Handicaps are always the most fiercely argued part of the Sexy Walrus E-Racing series, and is a hot topic of debate between the captains. Largely over whether Teo should have 50 extra kg added, or just 45.
+    Handicaps are always the most fiercely argued part of the Sexy Walrus E-Racing series, and is a hot topic of debate between the captains. Largely over whether Teo should 
+    have 50 extra kg added, or just 45.
 
     They are designed to make the racing as even as possible, penalising the strongest riders with worse bikes and extra weight. And even then Teo usually still wins.
 
-    Handicaps based largely off the end of last year’s season. And everyone always says “but I’m not as fit as I was then!” but that’s the case for everyone. So they are being kept like that, unless there’s some significant thing (eg broken leg) which might mean the handicap might change. 
+    Handicaps based largely off the end of last year’s season. And everyone always says “but I’m not as fit as I was then!” but that’s the case for everyone. So they are being
+    kept like that, unless there’s some significant thing (eg broken leg) which might mean the handicap might change. 
 
     After each round, handicaps are again fiercely fought over by the captains, with adjustments made to riders weights and bikes. 
 

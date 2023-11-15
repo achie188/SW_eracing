d69b19fbcdcf7426c95c600d45cbbcd216da54a2
Graeme Acheson <graeme@sports4cast.com>
2023-11-15 21:01:23 +0000

    update

--- pipeline/calcs.py
+++ pipeline/calcs.py
@@ -133,6 +133,9 @@ def calc_overall_pts(pro, s1, s2, s3, s4, s5, s6):
     # Concatenate the DataFrames
     all_dataframes = [pro, s1, s2, s3, s4, s5, s6]
     combined_df = pd.concat(all_dataframes, ignore_index=True)
+    combined_df['Total'] = pd.to_numeric(combined_df['Total'], errors='coerce')
+    combined_df['Total'].fillna(0, inplace=True)
+
 
 
 

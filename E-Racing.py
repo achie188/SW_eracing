8dde9a614b08cdfbf137780c9e4ebdbbf1d5c333
Graeme Acheson <graeme@sports4cast.com>
2023-11-15 22:12:17 +0000

    update

--- E-Racing.py
+++ E-Racing.py
@@ -40,7 +40,7 @@ s1_azt, s1_tesla, s2_tesla = race_reports()
 
 #Get stage data
 s1, orange_df = get_stage(zwift_ids[0], "Stage_1", ath_ids, "No")
-s2, orange_df = get_stage(zwift_ids[1], "Stage_2", ath_ids, "Yes", orange_df, )
+s2, orange_df = get_stage(zwift_ids[1], "Stage_2", ath_ids, "No", orange_df, )
 s3, orange_df = get_stage(zwift_ids[2], "Stage_3", ath_ids, "No", orange_df)
 s4, orange_df = get_stage(zwift_ids[3], "Stage_4", ath_ids, "No", orange_df)
 s5, orange_df = get_stage(zwift_ids[4], "Stage_5", ath_ids, "No", orange_df)

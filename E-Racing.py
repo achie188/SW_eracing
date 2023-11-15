a323581d177d8b184d7df9ba75e7aa0c90a445d0
Graeme Acheson <graeme@sports4cast.com>
2023-11-15 16:08:59 +0000

    update

--- E-Racing.py
+++ E-Racing.py
@@ -11,6 +11,7 @@ from inputs.pull_zwift import pull_zwift
 from inputs.helpers import get_ids
 from inputs.rules import rules
 from inputs.press_releases import press_releases
+from inputs.race_reports import race_reports
 from pipeline.formatting import get_zwift_ids, final_format, teams_slice
 from pipeline.calcs import get_stage, calc_overall_pts, calc_overall_orange, handicaps_format
 
@@ -32,8 +33,10 @@ zwift_ids = get_zwift_ids(stage_values, stages)
 
 handicaps = handicaps_format(handicaps)
 
+#Get text
 r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, handicaps_rules = rules()
 lego_pr, tesla_pr, azt_pr, abs_pr = press_releases()
+s1_tesla, s1, azt = race_reports()
 
 #Get stage data
 s1, orange_df = get_stage(zwift_ids[0], "Stage_1", ath_ids)
@@ -282,42 +285,9 @@ with tab2:
         with col2:
             st.subheader('Race Reports')
             with st.expander("Team Tesla"):
-                st.markdown('''
-                            #### Race Report - Tesla
-                            
-                            Go go go. George Dix is flying at the front of the race. His team behind them. They look amazing in their Amazon Prime blue jerseys. Dixy is so angry you can tell by looking at his electronic avatar. Why the hell are they going so fast.
-
-                            Meanwhile the Tesla Thames Water team with sponsors hell bent on rehabilitating their reputation look formidable in our red T jerseys! Looking comfortable we were all present and corrrect with Scottie up in the break feeding us live information on Chris Monk’s state of mind. 
-
-                            We appproached Mount doom looming in the distance like a bunch of super charged hobbits who have got to get back to the shire in time for Old Gaffer Gamgee’s mutton eating competition.
-
-                            The sprint came and went with no one on our team clear where the sprint was and our main sprinter Edwin unable to hear the rest of us which led to a comical if not frustrating 3 mins of people shouting into the wind and Ed smith sprinting at anything he could see.
-
-                            Anyway the real quiz came along pretty sharpish and the pack was set. About half way some bastard launched themselves and the pack was split and only one TTW was in the front group, me. Scottie was quickly gobbled up and spat out and up ahead Chris Monk continued to forge on in his skin tight orange jersey. 
-
-                            We reached the top and toppled over the edge. I found myself in a group of 2 chasing Tom and Chris up ahead. I worked as hard as I could to stave off any chance of the group behind catching and James Warman sat neatly behind like Frodo to my Sam. We were catching Chris but would run out of road and so we were fighting for 3rd place. Just like in the books at the last minute Frodo snuck past Sam and took the win. 
-
-                            The remainder of the TTWs fellowship rolled in happily as a group bruised but in no way beaten. 
-
-                            The old truism was muttered over comms..
-                            ‘The winners of week 1 have never won the season!’ 
-
-                            Someone belched loudly''')
+                st.markdown(s1_tesla)
             with st.expander("Team AstraZenaca"):
-                st.markdown('''
-                            #### Race Report - AstraZenaca
-                            
-                            A tough night for the Trailfinders.
-
-                            Camp was very upbeat leading up to the season opener. The new faces had settled in nicely and they’d put together a more than respectable set of prologue times. And instead of the habitual pre-race panic, there was an air of serenity in the group. Confidence was high.
-                            
-                            New signing Foley, in particular, was chomping at the bit to prove his worth to the sponsors. He was in great shape and he knew it. He was straight out into the breakaway, going toe to toe with the mercurial Monkfish. 
-
-                            Unfortunately, the rest of the team were no match for ‘Foley Fever’. Friend, Lopez and Tyler looked well short of last season’s form. The diamonds they once had in their legs, had turned into thick, gloopy, treacle. Normally so reliable in the sprint, they were caught at the back of the peloton reminiscing over former glories. Singh did fight hard on the climb, but his lungs were soon full of toxic levels of Volcanic ash and that was his day done. Wilson was perhaps the bravest of the Trailfinders tonight – this was her first race as a professional, and she was shown no mercy by the cut-throat Amazonian prime captain, Dix. 
-
-                            Lego Boots – with the race director in their ranks – were unstoppable tonight and deserve real credit. With their big budget and unlimited supply of baby oil, they’ve cemented their reputation as the bookie’s favourites.  Foley, Tesla captain Humphreys, and Amazon debutant Warmachine – were all up there at the business end…but they couldn’t quite get hold of those slippery Boots out front. 
-
-                            It'll be a restless night for the AZ Trailfinders there’s no doubt about it. But we’re only in week 1. And as the old saying goes, you can’t lose it in week 1. Can you?''')
+                st.markdown(s1_azt)
 
 
 
---
+++ inputs/race_reports.py
@@ -0,0 +1,42 @@
+def race_reports():
+    
+    s1_azt = '''
+    #### Race Report - AstraZenaca
+
+    A tough night for the Trailfinders.
+
+    Camp was very upbeat leading up to the season opener. The new faces had settled in nicely and they’d put together a more than respectable set of prologue times. And instead of the habitual pre-race panic, there was an air of serenity in the group. Confidence was high.
+
+    New signing Foley, in particular, was chomping at the bit to prove his worth to the sponsors. He was in great shape and he knew it. He was straight out into the breakaway, going toe to toe with the mercurial Monkfish. 
+
+    Unfortunately, the rest of the team were no match for ‘Foley Fever’. Friend, Lopez and Tyler looked well short of last season’s form. The diamonds they once had in their legs, had turned into thick, gloopy, treacle. Normally so reliable in the sprint, they were caught at the back of the peloton reminiscing over former glories. Singh did fight hard on the climb, but his lungs were soon full of toxic levels of Volcanic ash and that was his day done. Wilson was perhaps the bravest of the Trailfinders tonight – this was her first race as a professional, and she was shown no mercy by the cut-throat Amazonian prime captain, Dix. 
+
+    Lego Boots – with the race director in their ranks – were unstoppable tonight and deserve real credit. With their big budget and unlimited supply of baby oil, they’ve cemented their reputation as the bookie’s favourites.  Foley, Tesla captain Humphreys, and Amazon debutant Warmachine – were all up there at the business end…but they couldn’t quite get hold of those slippery Boots out front. 
+
+    It'll be a restless night for the AZ Trailfinders there’s no doubt about it. But we’re only in week 1. And as the old saying goes, you can’t lose it in week 1. Can you?
+    '''
+
+    s1_tesla = '''
+    #### Race Report - Tesla
+
+    Go go go. George Dix is flying at the front of the race. His team behind them. They look amazing in their Amazon Prime blue jerseys. Dixy is so angry you can tell by looking at his electronic avatar. Why the hell are they going so fast.
+
+    Meanwhile the Tesla Thames Water team with sponsors hell bent on rehabilitating their reputation look formidable in our red T jerseys! Looking comfortable we were all present and corrrect with Scottie up in the break feeding us live information on Chris Monk’s state of mind. 
+
+    We appproached Mount doom looming in the distance like a bunch of super charged hobbits who have got to get back to the shire in time for Old Gaffer Gamgee’s mutton eating competition.
+
+    The sprint came and went with no one on our team clear where the sprint was and our main sprinter Edwin unable to hear the rest of us which led to a comical if not frustrating 3 mins of people shouting into the wind and Ed smith sprinting at anything he could see.
+
+    Anyway the real quiz came along pretty sharpish and the pack was set. About half way some bastard launched themselves and the pack was split and only one TTW was in the front group, me. Scottie was quickly gobbled up and spat out and up ahead Chris Monk continued to forge on in his skin tight orange jersey. 
+
+    We reached the top and toppled over the edge. I found myself in a group of 2 chasing Tom and Chris up ahead. I worked as hard as I could to stave off any chance of the group behind catching and James Warman sat neatly behind like Frodo to my Sam. We were catching Chris but would run out of road and so we were fighting for 3rd place. Just like in the books at the last minute Frodo snuck past Sam and took the win. 
+
+    The remainder of the TTWs fellowship rolled in happily as a group bruised but in no way beaten. 
+
+    The old truism was muttered over comms..
+    ‘The winners of week 1 have never won the season!’ 
+
+    Someone belched loudly
+    '''
+
+    return s1_azt, s1_tesla

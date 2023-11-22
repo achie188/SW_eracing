e43069781d1f490c1c7c968b95953249751a5999
Graeme Acheson <graeme@sports4cast.com>
2023-11-22 23:51:40 +0000

    update

--- E-Racing.py
+++ E-Racing.py
@@ -18,7 +18,7 @@ from pipeline.calcs import get_stage, calc_overall_pts, calc_overall_orange, han
 
 
 # Manual overrides
-refresh_interval = 60
+refresh_interval = 300
 stages_complete = ['Prologue', 'Stage 1', 'Stage 2', 'Stage 3']
 
 

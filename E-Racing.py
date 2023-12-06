245034af734f7c9d9799f34dcd51f1c656e63f86
Graeme Acheson <graeme@sports4cast.com>
2023-12-06 17:59:26 +0000

    stage 4 test

--- E-Racing.py
+++ E-Racing.py
@@ -74,12 +74,9 @@ s4 = final_format(s4)
 s5 = final_format(s5)
 s6 = final_format(s6)
 
-live = ttt_team
+
 #Get live event
-# if not live.empty:
-#     live = format_results(live, ath_ids)
-#     live.drop(columns=['Time_secs'], inplace=True)
-#     live = live.rename(columns={'Time_nice': 'Time'})
+live = get_stage('3979927')
 
 
 #Team slices
@@ -128,18 +125,12 @@ tab1, tab2, tab3, tab4 = st.tabs(["Championship", "All Results", "About", "LIVE 
                                                     ###############################
 
 with tab4:
-    col1, col2 = st.columns(2)
 
-    with col1:
-        if live is not None and not live.empty:
-            st.subheader('Team Live Positions')
-            st.dataframe(ttt_team, height = int(35.2*(ttt_team.shape[0]+1)), hide_index=True)
-        else:
-            st.write("No live data right now.")
-    with col2:
-        if live is not None and not live.empty:
-            st.subheader('Individual Live Positions')
-            st.dataframe(ttt_ind, height = int(35.2*(ttt_ind.shape[0]+1)), hide_index=True)
+    if live is not None and not live.empty:
+        st.subheader('Live Race')
+        st.dataframe(live, height = int(35.2*(live.shape[0]+1)), hide_index=True)
+    else:
+        st.write("No live data right now.")
    
 
 

747253d537c392fce84ca79022f798f22ade2d17
Graeme Acheson <graeme@sports4cast.com>
2023-12-06 18:06:32 +0000

    update

--- E-Racing.py
+++ E-Racing.py
@@ -12,7 +12,7 @@ from inputs.rules import rules
 from inputs.press_releases import press_releases
 from inputs.race_reports import race_reports
 from inputs.pull_zwift import pull_zwift
-from pipeline.formatting import get_zwift_ids, final_format, teams_slice
+from pipeline.formatting import get_zwift_ids, final_format, teams_slice, format_results
 from pipeline.calcs import get_stage, calc_overall_pts, calc_overall_orange, handicaps_format
 from pipeline.ttt import sort_ttt
 
@@ -78,6 +78,7 @@ s6 = final_format(s6)
 
 #Get live event
 live = pull_zwift('3979927')
+live = format_results(live, ath_ids)
 
 
 #Team slices
